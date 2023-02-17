413e85295a927efa9248d038d228adbc09b8f3d1
Sergey_Kryuchkov <123768464+SergeyKruchkov@users.noreply.github.com>
2023-02-18 00:32:08 +0300

    Add files via upload

---
+++ 2112/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _2112
+{
+    internal class Program
+     {
+        static void Main()
+        {
+            int a;
+            Console.WriteLine("Введите a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            if((a % 3 == 0)&&(a % 5 == 0))
+            {
+                Console.WriteLine("fezzbuzz");
+            }
+            else if (a % 5 == 0)
+            {
+                Console.WriteLine("buzz");
+            }
+            else if(a % 3 == 0) {
+                Console.WriteLine("fizz");
+            }
+        }
+    }
+}
